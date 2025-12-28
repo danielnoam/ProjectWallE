696bd9d16dc0fa15052e20189dc9e8fc889e9b99
ShmueLapanim <tomeriko0011@gmail.com>
2025-12-28 21:30:46 +0200

    Added tire friction (WIP)

--- Assets/2_Scripts/CarController.cs
+++ Assets/2_Scripts/CarController.cs
@@ -10,19 +10,34 @@ public class CarController : MonoBehaviour
     public float suspensionStrength;
     public float suspensionDamping;
     
+    [Header("Steering Parameters")]
+    public float steeringAngle;
+    public float steeringStrength;
+    [Range(0,1)] public float tireGripFactor;
+    
     [Header("Acceleration Parameters")]
     public float accelerationStrength;
     public float brakeStrength;
     
-    private Rigidbody _rb;
+    private Rigidbody _carRb;
+    private Transform _frontRightTire;
+    private Transform _frontLeftTire;
+    
+    float _currentSteering;
+
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _carRb = GetComponent<Rigidbody>();
+        
+        _frontRightTire = tireTransforms[0];
+        _frontLeftTire = tireTransforms[1];
     }
     void FixedUpdate()
     {
         ApplySuspension();
+        ApplyTireRotation();
+        ApplyTireFriction();
         ApplyAcceleration();
     }
 
@@ -35,11 +50,47 @@ public class CarController : MonoBehaviour
             
             float offset = tireHit.distance - groundHeight;
             
-            float suspensionForce = (-offset * suspensionStrength) - (_rb.GetPointVelocity(tire.position).y * suspensionDamping);
+            float suspensionForce = (-offset * suspensionStrength) - (_carRb.GetPointVelocity(tire.position).y * suspensionDamping);
             
-            _rb.AddForceAtPosition(tire.up * suspensionForce, tire.position);
+            _carRb.AddForceAtPosition(tire.up * suspensionForce, tire.position);
         }
     }
+
+    private void ApplyTireRotation()
+    {
+        float target = Input.GetAxis("Horizontal") * steeringAngle;
+
+        _currentSteering = Mathf.MoveTowards(
+            _currentSteering,
+            target,
+            steeringStrength * Time.fixedDeltaTime
+        );
+
+        Quaternion rot = Quaternion.Euler(0f, _currentSteering, 0f);
+
+        _frontLeftTire.localRotation = rot;
+        _frontRightTire.localRotation = rot;
+    }
+
+    private void ApplyTireFriction()
+    {
+        foreach (Transform t in tireTransforms)
+        {
+            bool isGrounded = Physics.Raycast(t.position, -t.up, out RaycastHit tireHit, groundHeight);
+            if (!isGrounded) continue;
+            
+            Vector3 tireVel = _carRb.GetPointVelocity(t.position);
+            
+            float steeringVel = Vector3.Dot(t.right, tireVel);
+            
+            float desiredVelChange = -steeringVel * tireGripFactor;
+            
+            float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
+            
+            _carRb.AddForceAtPosition(t.right * _carRb.mass/tireTransforms.Length * desiredAccel, t.position);
+        }
+    }
+
 
     void ApplyAcceleration()
     {
@@ -50,7 +101,7 @@ public class CarController : MonoBehaviour
                 bool isGrounded = Physics.Raycast(tire.position, -tire.up, out _, groundHeight);
                 if (!isGrounded) continue;
                 
-                _rb.AddForceAtPosition(tire.forward * accelerationStrength, tire.position);
+                _carRb.AddForceAtPosition(tire.forward * accelerationStrength, tire.position);
             }
         }
         
@@ -60,7 +111,7 @@ public class CarController : MonoBehaviour
             {
                 bool isGrounded = Physics.Raycast(tire.position, -tire.up, out _, groundHeight);
                 if (!isGrounded) continue;
-                _rb.AddForceAtPosition(-tire.forward * brakeStrength, tire.position);
+                _carRb.AddForceAtPosition(-tire.forward * brakeStrength, tire.position);
             }
         }
     }
