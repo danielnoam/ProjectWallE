07710e59932990956b66e8d1502db028b8c9ebd2
Daniel-PC <danielnoam1999@gmail.com>
2025-12-28 19:10:13 +0200

    Testing virtual visuals

--- Assets/2_Scripts/BaseBuilding/Structures/Base.cs
+++ Assets/2_Scripts/BaseBuilding/Structures/Base.cs
@@ -1,7 +1,23 @@
+using System;
 using UnityEngine;
 
 public class Base : Structure
 {
+
+    [Header("Base Settings")]
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private Transform obelisk;
+
+
+    
+    
+    private void Update()
+    {
+        obelisk.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        float sine = Mathf.Sin(Time.time * 2f) * moveSpeed;
+        obelisk.localPosition = new Vector3(0f, sine, 0f);
+    }
 
 
     protected override void OnBuild()
