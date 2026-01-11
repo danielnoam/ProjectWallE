f5426b4d63c065da2de0dcf65da8f5b11ef0a5ed
Daniel-PC <danielnoam1999@gmail.com>
2026-01-11 18:48:44 +0200

    Update
    added line renderer for the building position

--- Assets/2_Scripts/GameLoop/PlayerStructureBuilder.cs
+++ Assets/2_Scripts/GameLoop/PlayerStructureBuilder.cs
@@ -1,3 +1,4 @@
+using DNExtensions;
 using UnityEngine;
 
 public class PlayerStructureBuilder : MonoBehaviour
@@ -5,22 +6,35 @@ public class PlayerStructureBuilder : MonoBehaviour
     [Header("Build Settings")]
     [SerializeField] private float buildRange = 100f;
     [SerializeField] private LayerMask buildableLayerMask;
+    [SerializeField] private LayerMask blockBuildLayerMask;
     [SerializeField] private KeyCode buildMenuKey = KeyCode.Mouse1;
     
     [Header("References")]
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private RadialMenu<Structure> radialMenu;
     [SerializeField] private Structure[] structuresArray;
 
+    private Ray _buildRay;
+    public bool CanBuild { get; private set; }
+    
+    
     private void OnValidate()
     {
         if (!mainCamera) mainCamera = Camera.main;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position);
     }
 
     private void Awake()
     {
         radialMenu.SetupMenu(structuresArray, ConfigureStructureElement);
         radialMenu.OnItemSelected += TryBuildStructure;
+    }
+    
+    private void OnDestroy()
+    {
+        radialMenu.OnItemSelected -= TryBuildStructure;
     }
 
     private void Update()
@@ -33,7 +47,33 @@ public class PlayerStructureBuilder : MonoBehaviour
         {
             radialMenu.CloseMenu();
         }
+        
+        CastBuildRay();
     }
+
+    private void CastBuildRay()
+    {
+        lineRenderer.SetPosition(0, transform.position.RemoveY(0.5f));
+        
+        _buildRay = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(_buildRay, buildRange, blockBuildLayerMask))
+        {
+            lineRenderer.SetPosition(1, transform.position + _buildRay.direction * buildRange);
+            CanBuild = false;
+        }
+        else if (Physics.Raycast(_buildRay, out RaycastHit hit, buildRange, buildableLayerMask))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+            CanBuild = true;
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.position + _buildRay.direction * buildRange);
+            CanBuild = false;
+        }
+    }
+    
+    
 
     private void ConfigureStructureElement(RadialMenuElement element, Structure structure)
     {
@@ -44,9 +84,13 @@ public class PlayerStructureBuilder : MonoBehaviour
 
     private void TryBuildStructure(Structure structure)
     {
-        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, buildRange, buildableLayerMask))
+        if (!CanBuild)
+        {
+            Debug.Log("Cannot build here");
+            return;
+        }
+        
+        if (!Physics.Raycast(_buildRay, out RaycastHit hit, buildRange, buildableLayerMask))
         {
             Debug.Log("No valid build surface found");
             return;
@@ -61,8 +105,5 @@ public class PlayerStructureBuilder : MonoBehaviour
         StructureDispatcher.Instance.DeployPod(structure, hit.point, hit.normal);
     }
 
-    private void OnDestroy()
-    {
-        radialMenu.OnItemSelected -= TryBuildStructure;
-    }
+
 }
