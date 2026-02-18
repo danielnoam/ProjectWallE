757725f547c9f8589390f229e494efe4c7cdcb69
ShmueLapanim <tomeriko0011@gmail.com>
2026-02-18 19:55:17 +0200

    update

--- Assets/2_Scripts/GameLoop/Player/RobotController.cs
+++ Assets/2_Scripts/GameLoop/Player/RobotController.cs
@@ -11,6 +11,7 @@ namespace ProjectWallE
         [SerializeField] Transform _groundRayPoint;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Rigidbody playerRB;
+        [SerializeField] private Transform cameraTransform;
         
         [Header("Grounded Settings")]
         [SerializeField] private float groundHeight = 0.5f;
@@ -33,16 +34,26 @@ namespace ProjectWallE
         {
             UpdateGroundHeight();
             UpdateLocomotion();
+            UpdateRotation();
         }
 
         #region Locomotion
 
         private void UpdateLocomotion()
-        {
-            Vector3 targetDir = new Vector3(_input.Movement.x, 0, _input.Movement.y);
-            
-            playerRB.AddForce(targetDir * moveAccel);
+        { 
+            Vector3 moveDir = GetCameraRelativeDirection(_input.Movement);
+            Vector3 targetVel = moveDir * maxMoveSpeed;
+
+            Vector3 currentVel = playerRB.linearVelocity;
+            Vector3 newVel = Vector3.MoveTowards(
+                new Vector3(currentVel.x, 0, currentVel.z),
+                targetVel,
+                moveAccel * Time.fixedDeltaTime
+            );
+
+            playerRB.linearVelocity = new Vector3(newVel.x, currentVel.y, newVel.z);
         }
+
 
         #endregion
         
@@ -65,6 +76,15 @@ namespace ProjectWallE
 
         #endregion
         
+        #region Rotation
+
+        private void UpdateRotation()
+        {
+            
+        }
+        
+        #endregion
+        
         #region Helpers
 
         private bool IsGrounded(out RaycastHit hit)
@@ -83,6 +103,28 @@ namespace ProjectWallE
             _isGrounded = groundedNow;
             return groundedNow;
         }
+        
+        private Vector3 GetCameraRelativeDirection(Vector2 input)
+        {
+            if (input.sqrMagnitude < 0.01f)
+                return Vector3.zero;
+
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+
+            // Flatten
+            camForward.y = 0f;
+            camRight.y = 0f;
+
+            camForward.Normalize();
+            camRight.Normalize();
+
+            Vector3 moveDir = camForward * input.y +
+                              camRight * input.x;
+
+            return moveDir.normalized;
+        }
+
 
 
         #endregion
