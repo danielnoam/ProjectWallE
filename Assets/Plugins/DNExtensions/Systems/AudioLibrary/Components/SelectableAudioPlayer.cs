8ac285fbe187104fb5078d7f2a0aac244d8b9235
Daniel-PC <danielnoam1999@gmail.com>
2026-04-18 15:55:07 +0300

    Update
    started working on main menu

--- Assets/Plugins/DNExtensions/Systems/AudioLibrary/Components/SelectableAudioPlayer.cs
+++ Assets/Plugins/DNExtensions/Systems/AudioLibrary/Components/SelectableAudioPlayer.cs
@@ -5,29 +5,36 @@ using UnityEngine.UI;
 
 namespace DNExtensions.Systems.AudioLibrary
 {
-    /// <summary>
-    /// Plays audio from the audio library on Selectable interaction events.
-    /// </summary>
     [DisallowMultipleComponent]
     [AddComponentMenu("DNExtensions/Audio Library/Selectable Audio Player")]
     public class SelectableAudioPlayer : MonoBehaviour
     {
-        [SerializeField] private bool onHover;
-        [SerializeField, AudioLibraryID] private string hoverAudioID = string.Empty;
-
-        [SerializeField] private bool onUnhover;
-        [SerializeField, AudioLibraryID] private string unhoverAudioID = string.Empty;
-
         [SerializeField] private bool onSelect;
         [SerializeField, AudioLibraryID] private string selectAudioID = string.Empty;
+
+        [SerializeField] private bool onDeselect;
+        [SerializeField, AudioLibraryID] private string deselectAudioID = string.Empty;
+
+        [SerializeField] private bool onSubmit;
+        [SerializeField, AudioLibraryID] private string submitAudioID = string.Empty;
 
         [SerializeField, HideInInspector, AutoGetSelf] private Selectable selectable;
 
         private void Awake()
         {
-            if (onHover)   selectable.OnPointerEnter(_ => Play(hoverAudioID));
-            if (onUnhover) selectable.OnPointerExit(_ => Play(unhoverAudioID));
-            if (onSelect)  selectable.OnSelect(_ => Play(selectAudioID));
+            if (onSelect)   selectable.OnSelect(_ => Play(selectAudioID));
+            if (onDeselect) selectable.OnDeselect(_ => Play(deselectAudioID));
+            if (onSubmit)
+            {
+                if (selectable is Button button)
+                {
+                    button.onClick.AddListener(() => Play(submitAudioID));
+                }
+                else
+                {
+                    selectable.OnSubmit(_ => Play(submitAudioID));
+                }
+            }
         }
 
         private static void Play(string audioID)
